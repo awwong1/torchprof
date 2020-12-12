a63d1334288fd6fdb2fb1d567b736448a7d02934
Alexander Wong <alex@udia.ca>
2020-12-11 23:56:50 -0700

    Backwards compatible memory profiling and `self_cuda_total`
    - refactored display code to separate file
    - warning if calling profile_memory on unsupported library
    - dynamically determine whether or not column should be displayed

--- torchprof/profile.py
+++ torchprof/profile.py
@@ -1,10 +1,12 @@
 import functools
-from itertools import groupby
-import torch.autograd.profiler as tprofiler
-from collections import namedtuple, defaultdict, OrderedDict
+import warnings
+from collections import OrderedDict, defaultdict, namedtuple
+
+import torch.autograd.profiler as torch_profiler
+
+from .display import traces_to_display
 
 Trace = namedtuple("Trace", ["path", "leaf", "module"])
-Measure = namedtuple("Measure", ["self_cpu_total", "cpu_total", "cuda_total", "cpu_memory", "cuda_memory", "occurrences"])
 
 
 def walk_modules(module, name="", path=()):
@@ -20,10 +22,11 @@ def walk_modules(module, name="", path=()):
 
 
 class Profile(object):
-    """Layer by layer profiling of Pytorch models, using the Pytorch autograd profiler.
-    """
+    """Layer by layer profiling of PyTorch models, using the PyTorch autograd profiler."""
 
-    def __init__(self, model, enabled=True, use_cuda=False, profile_memory=False, paths=None):
+    def __init__(
+        self, model, enabled=True, use_cuda=False, profile_memory=False, paths=None
+    ):
         self._model = model
         self.enabled = enabled
         self.use_cuda = use_cuda
@@ -53,11 +56,7 @@ class Profile(object):
         self.exited = True
 
     def __str__(self):
-        if self.exited:
-            return traces_to_display(
-                self.traces, self.trace_profile_events, paths=self.paths
-            )
-        return "<unfinished torchprof.profile>"
+        return self.display()
 
     def __call__(self, *args, **kwargs):
         return self._model(*args, **kwargs)
@@ -72,8 +71,21 @@ class Profile(object):
 
             @functools.wraps(_forward)
             def wrap_forward(*args, **kwargs):
-                with tprofiler.profile(use_cuda=self.use_cuda, profile_memory=self.profile_memory) as prof:
-                    res = _forward(*args, **kwargs)
+                try:
+                    with torch_profiler.profile(
+                        use_cuda=self.use_cuda, profile_memory=self.profile_memory
+                    ) as prof:
+                        res = _forward(*args, **kwargs)
+                except TypeError:
+                    if self.profile_memory:
+                        warnings.warn(
+                            "`profile_memory` is unsupported in torch < 1.6",
+                            RuntimeWarning,
+                        )
+                        self.profile_memory = False
+                    with torch_profiler.profile(use_cuda=self.use_cuda) as prof:
+                        res = _forward(*args, **kwargs)
+
                 event_list = prof.function_events
                 event_list.populate_cpu_children()
                 # each profile call should be contained in its own list
@@ -101,117 +113,7 @@ class Profile(object):
                 self.trace_profile_events,
                 show_events=show_events,
                 paths=self.paths,
+                use_cuda=self.use_cuda,
+                profile_memory=self.profile_memory,
             )
         return "<unfinished torchprof.profile>"
-
-
-def flatten_tree(t, depth=0):
-    flat = []
-    for name, st in t.items():
-        measures = st.pop(None, None)
-        flat.append([depth, name, measures])
-        flat.extend(flatten_tree(st, depth=depth + 1))
-    return flat
-
-
-def traces_to_display(traces, trace_events, show_events=False, paths=None):
-    """Construct human readable output of the profiler traces and events.
-    """
-    tree = OrderedDict()
-
-    for trace in traces:
-        [path, leaf, module] = trace
-        current_tree = tree
-        # unwrap all of the events, in case model is called multiple times
-        events = [te for tevents in trace_events[path] for te in tevents]
-        for depth, name in enumerate(path, 1):
-            if name not in current_tree:
-                current_tree[name] = OrderedDict()
-            if depth == len(path) and (
-                (paths is None and leaf) or (paths is not None and path in paths)
-            ):
-                # tree measurements have key None, avoiding name conflict
-                if show_events:
-                    for event_name, event_group in groupby(events, lambda e: e.name):
-                        event_group = list(event_group)
-                        current_tree[name][event_name] = {
-                            None: Measure(
-                                sum([e.self_cpu_time_total for e in event_group]),
-                                sum([e.cpu_time_total for e in event_group]),
-                                sum([e.cuda_time_total for e in event_group]),
-                                sum([e.cpu_memory_usage for e in event_group]),
-                                sum([e.cuda_memory_usage for e in event_group]),
-                                len(event_group)
-                            )
-                        }
-                else:
-                    current_tree[name][None] = Measure(
-                        sum([e.self_cpu_time_total for e in events]),
-                        sum([e.cpu_time_total for e in events]),
-                        sum([e.cuda_time_total for e in events]),
-                        sum([e.cpu_memory_usage for e in events]),
-                        sum([e.cuda_memory_usage for e in events]),
-                        len(trace_events[path])
-                    )
-            current_tree = current_tree[name]
-    tree_lines = flatten_tree(tree)
-
-    # dt = ('|', '|-- ', '+-- ', ' ') # ascii
-    dt = ("\u2502", "\u251c\u2500\u2500 ", "\u2514\u2500\u2500 ", " ")  # ascii-ex
-    format_lines = []
-    for idx, tree_line in enumerate(tree_lines):
-        depth, name, measures = tree_line
-        self_cpu_time = ""
-        cpu_time = ""
-        cuda_time = ""
-        cpu_memory = ""
-        cuda_memory = ""
-        occurrences = ""
-        if measures:
-            self_cpu_time = tprofiler.format_time(measures.self_cpu_total)
-            cpu_time = tprofiler.format_time(measures.cpu_total)
-            cuda_time = tprofiler.format_time(measures.cuda_total)
-            cpu_memory = tprofiler.format_memory(measures.cpu_memory)
-            cuda_memory = tprofiler.format_memory(measures.cuda_memory)
-            occurrences = str(measures.occurrences)
-        pre = ""
-        next_depths = [pl[0] for pl in tree_lines[idx + 1 :]]
-        current = True
-        while depth:
-            if current:
-                if depth in next_depths and next_depths[0] >= depth:
-                    pre = dt[1]
-                else:
-                    pre = dt[2]
-            else:
-                if depth in next_depths:
-                    pre = dt[0] + pre
-                else:
-                    pre = dt[3] + pre
-            depth -= 1
-            current = False
-        format_lines.append([pre + name, self_cpu_time, cpu_time, cuda_time, cpu_memory, cuda_memory, occurrences])
-
-    # construct the table
-    heading = ("Module", "Self CPU total", "CPU total", "CUDA total", "CPU mem total", "CUDA mem total", "Occurrences")
-    max_lens = [max(map(len, col)) for col in zip(*([heading] + format_lines))]
-    # create the heading
-    disp = "{:<{}s}".format(heading[0], max_lens[0]) + " | "
-    disp += "{:>{}s}".format(heading[1], max_lens[1]) + " | "
-    disp += "{:>{}s}".format(heading[2], max_lens[2]) + " | "
-    disp += "{:>{}s}".format(heading[3], max_lens[3]) + " | "
-    disp += "{:>{}s}".format(heading[4], max_lens[4]) + " | "
-    disp += "{:>{}s}".format(heading[5], max_lens[5]) + " | "
-    disp += "{:>{}s}".format(heading[6], max_lens[6]) + "\n"
-    disp += "-|-".join(["-" * mlen for mlen in max_lens]) + "\n"
-    for line in format_lines:
-        label, self_cpu_time, cpu_time, cuda_time, cpu_memory, cuda_memory, occurrences = line
-        disp += "{:<{}s}".format(label, max_lens[0]) + " | "
-        disp += "{:>{}s}".format(self_cpu_time, max_lens[1]) + " | "
-        disp += "{:>{}s}".format(cpu_time, max_lens[2]) + " | "
-        disp += "{:>{}s}".format(cuda_time, max_lens[3]) + " | "
-        disp += "{:>{}s}".format(cpu_memory, max_lens[4]) + " | "
-        disp += "{:>{}s}".format(cuda_memory, max_lens[5]) + " | "
-        disp += "{:>{}s}".format(occurrences, max_lens[6]) + "\n"
-
-    return disp
